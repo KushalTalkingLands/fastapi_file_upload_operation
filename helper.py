ad12d3058bf7eef97051c03ab60fc8c534ea1f3c
unknown <kushal@talkinglands.com>
2024-04-01 11:54:46 +0530

    Added file Upload in fast api with validation of file size check

--- helper.py
+++ helper.py
@@ -9,8 +9,9 @@ def validate_file(file: UploadFile):
     accepted_extensions = os.getenv("ACCEPTED_EXTENSIONS")
     display_accepted_extensions = accepted_extensions
     accepted_file_prefix = os.getenv("ACCEPTED_FILENAME_PREFIX")
-    print(accepted_extensions)
-    print(display_accepted_extensions)
+    # print(accepted_extensions)
+    # print(display_accepted_extensions)
+    # print("size check",file.file.seek(0, os.SEEK_END))
     if not accepted_extensions:
         raise ValueError("ACCEPTED_EXTENSIONS environment variable is not set")
     
@@ -23,3 +24,5 @@ def validate_file(file: UploadFile):
     ext = os.path.splitext(file.filename)[1].lower()
     if ext not in accepted_extensions:
         raise HTTPException(status_code=400, detail=f"File extension must be  '{display_accepted_extensions}'")
+    if file.file.seek(0, os.SEEK_END) == 0:
+        raise HTTPException(status_code=400, detail="File is empty")
